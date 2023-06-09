72a534381769380a8a4a5f8fbe12d40a39676508
KovaKreative <eric.kovalevskyy@gmail.com>
2023-06-09 17:06:41 -0400

    Implement faqs page

--- src/app/faq/faq.component.ts
+++ src/app/faq/faq.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-faq',
   templateUrl: './faq.component.html',
   styleUrls: ['./faq.component.sass']
 })
-export class FaqComponent {
+export class FaqComponent implements OnInit {
 
+  faqs: Array<any> = [];
+
+  constructor(private http: HttpClient) {
+    this.http.get('https://jsonplaceholder.typicode.com/posts', {responseType: 'json'})
+    .subscribe((res:any) => {
+      this.faqs = res.slice(0,9);
+    });
+
+  }
+
+  ngOnInit() {
+
+  }
 }
